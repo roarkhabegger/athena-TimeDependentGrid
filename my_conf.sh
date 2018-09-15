6ff1ba81cbae200aa74c0ad29b70f7d56f9e24c9
John Dupuy <jdupuy26@gmail.com>
2018-09-15 11:52:20 -0400

    Updated configuration script

--- my_conf.sh
+++ my_conf.sh
@@ -2,11 +2,14 @@
 python configure.py\
 			 --prob blast\
 			 --flux roe\
-			 --nghost=3 \
+			 --coord cartesian \
+			 --nghost=2 \
 			 -debug \
 			 -hdf5 \
-			 --include /usr/include/hdf5/serial/ \
+			 --include /usr/include/hdf5/openmpi/ \
 			 --fluxcl hlle\
 			 -cl \
+			 -mpi \
+			 -omp 
 			 #--include /usr/include/hdf5/openmpi/ \
 #			 --hdf5_path=/usr/lib/x86_64-linux-gnu/
